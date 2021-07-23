8921c8c0fa748978b166bf10fc7fd4c74435e518
gerardimitri <gtrincadomunoz@gmail.com>
2021-07-23 02:10:18 -0400

    Imports

---
+++ pool_party.py
@@ -0,0 +1,18 @@
+import glfw
+from OpenGL.GL import *
+import OpenGL.GL.shaders
+import numpy as np
+import random
+import sys
+import os.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import grafica.transformations as tr
+import grafica.basic_shapes as bs
+import grafica.easy_shaders as es
+import grafica.lighting_shaders as ls
+import grafica.performance_monitor as pm
+from grafica.assets_path import getAssetPath
+
+__author__ = "Gerardo Trincado"
+__license__ = "MIT"
+
