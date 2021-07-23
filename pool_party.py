b581769cd6c10c8bb77315b41e843f181d60169e
gerardimitri <gtrincadomunoz@gmail.com>
2021-07-23 03:26:23 -0400

    cambios textura y obj

--- pool_party.py
+++ pool_party.py
@@ -16,7 +16,7 @@ from grafica.assets_path import getAssetPath
 __author__ = "Gerardo Trincado"
 __license__ = "MIT"
 
- A class to store the application control
+#A class to store the application control
 class Controller:
     def __init__(self):
         self.fillPolygon = True
@@ -60,7 +60,7 @@ def readFaceVertex(faceDescription):
 
 
 
-def readOBJ(filename, color):
+def readOBJ(filename, textureFileName):
 
     vertices = []
     normals = []
@@ -110,4 +110,154 @@ def readOBJ(filename, color):
             indices += [index, index + 1, index + 2]
             index += 3        
 
-        return bs.Shape(vertexData, indices)
+        return bs.Shape(vertexData, indices, textureFileName)
+
+
+
+
+
+if __name__ == "__main__":
+
+    # Initialize glfw
+    if not glfw.init():
+        glfw.set_window_should_close(window, True)
+
+    width = 600
+    height = 600
+    title = "Reading a *.obj file"
+    window = glfw.create_window(width, height, title, None, None)
+
+    if not window:
+        glfw.terminate()
+        glfw.set_window_should_close(window, True)
+
+    glfw.make_context_current(window)
+
+    # Connecting the callback function 'on_key' to handle keyboard events
+    glfw.set_key_callback(window, on_key)
+
+    # Defining shader programs
+    pipeline = ls.SimpleGouraudShaderProgram()
+    phongPipeline = ls.SimpleTexturePhongShaderProgram()
+    mvpPipeline = es.SimpleModelViewProjectionShaderProgram()
+
+    # Telling OpenGL to use our shader program
+    glUseProgram(pipeline.shaderProgram)
+
+    # Setting up the clear screen color
+    glClearColor(0.85, 0.85, 0.85, 1.0)
+
+    # As we work in 3D, we need to check which part is in front,
+    # and which one is at the back
+    glEnable(GL_DEPTH_TEST)
+
+    # Convenience function to ease initialization
+    def createGPUShape(pipeline, shape):
+        gpuShape = es.GPUShape().initBuffers()
+        pipeline.setupVAO(gpuShape)
+        gpuShape.fillBuffers(shape.vertices, shape.indices, GL_STATIC_DRAW)
+        return gpuShape
+
+    # Creating shapes on GPU memory
+    gpuAxis = createGPUShape(mvpPipeline, bs.createAxis(7))
+
+    shapePoolTable = readOBJ('pool_table2.obj', 'pool_table.jpg')
+    gpuPoolTable = createGPUShape(pipeline, shapePoolTable)
+    gpuPoolTable.texture = es.textureSimpleSetup('pool_table.jpg', GL_REPEAT, GL_REPEAT, GL_LINEAR, GL_LINEAR)
+
+
+    # Setting uniforms that will NOT change on each iteration
+    glUseProgram(phongPipeline.shaderProgram)
+    glUniform3f(glGetUniformLocation(phongPipeline.shaderProgram, "La"), 1.0, 1.0, 1.0)
+    glUniform3f(glGetUniformLocation(phongPipeline.shaderProgram, "Ld"), 1.0, 1.0, 1.0)
+    glUniform3f(glGetUniformLocation(phongPipeline.shaderProgram, "Ls"), 1.0, 1.0, 1.0)
+
+    glUniform3f(glGetUniformLocation(phongPipeline.shaderProgram, "Ka"), 0.2, 0.2, 0.2)
+    glUniform3f(glGetUniformLocation(phongPipeline.shaderProgram, "Kd"), 0.9, 0.9, 0.9)
+    glUniform3f(glGetUniformLocation(phongPipeline.shaderProgram, "Ks"), 1.0, 1.0, 1.0)
+
+    glUniform3f(glGetUniformLocation(phongPipeline.shaderProgram, "lightPosition"), -3, 0, 3)
+    
+    glUniform1ui(glGetUniformLocation(phongPipeline.shaderProgram, "shininess"), 100)
+    glUniform1f(glGetUniformLocation(phongPipeline.shaderProgram, "constantAttenuation"), 0.001)
+    glUniform1f(glGetUniformLocation(phongPipeline.shaderProgram, "linearAttenuation"), 0.1)
+    glUniform1f(glGetUniformLocation(phongPipeline.shaderProgram, "quadraticAttenuation"), 0.01)
+
+    # Setting up the projection transform
+    projection = tr.perspective(60, float(width)/float(height), 0.1, 100)
+    glUniformMatrix4fv(glGetUniformLocation(phongPipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
+
+    glUseProgram(mvpPipeline.shaderProgram)
+    glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "projection"), 1, GL_TRUE, projection)
+    glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "model"), 1, GL_TRUE, tr.identity())
+
+    t0 = glfw.get_time()
+    camera_theta = -3*np.pi/4
+
+    perfMonitor = pm.PerformanceMonitor(glfw.get_time(), 0.5)
+
+    # glfw will swap buffers as soon as possible
+    glfw.swap_interval(0)
+
+    while not glfw.window_should_close(window):
+
+        # Measuring performance
+        perfMonitor.update(glfw.get_time())
+        glfw.set_window_title(window, title + str(perfMonitor))
+
+        # Using GLFW to check for input events
+        glfw.poll_events()
+
+        # Getting the time difference from the previous iteration
+        t1 = glfw.get_time()
+        dt = t1 - t0
+        t0 = t1
+
+        if (glfw.get_key(window, glfw.KEY_LEFT) == glfw.PRESS):
+            camera_theta -= 2 * dt
+
+        if (glfw.get_key(window, glfw.KEY_RIGHT) == glfw.PRESS):
+            camera_theta += 2* dt
+
+        # Setting up the view transform
+        R = 12
+        camX = R * np.sin(camera_theta)
+        camY = R * np.cos(camera_theta)
+        viewPos = np.array([camX, camY, 7])
+        view = tr.lookAt(
+            viewPos,
+            np.array([0,0,1]),
+            np.array([0,0,1])
+        )
+
+        # Clearing the screen in both, color and depth
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # Filling or not the shapes depending on the controller state
+        if (controller.fillPolygon):
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+        else:
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+
+        # Drawing shapes
+        glUseProgram(phongPipeline.shaderProgram)
+        glUniform3f(glGetUniformLocation(phongPipeline.shaderProgram, "viewPosition"), viewPos[0], viewPos[1], viewPos[2])
+        glUniformMatrix4fv(glGetUniformLocation(phongPipeline.shaderProgram, "view"), 1, GL_TRUE, view)
+
+        glUniformMatrix4fv(glGetUniformLocation(phongPipeline.shaderProgram, "model"), 1, GL_TRUE, tr.uniformScale(3))
+        phongPipeline.drawCall(gpuPoolTable)
+
+        
+        glUseProgram(mvpPipeline.shaderProgram)
+        glUniformMatrix4fv(glGetUniformLocation(mvpPipeline.shaderProgram, "view"), 1, GL_TRUE, view)
+        mvpPipeline.drawCall(gpuAxis, GL_LINES)
+
+        # Once the drawing is rendered, buffers are swap so an uncomplete drawing is never seen.
+        glfw.swap_buffers(window)
+
+    # freeing GPU memory
+    gpuAxis.clear()
+    gpuPoolTable.clear()
+
+    glfw.terminate()
+
