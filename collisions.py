4e37226d17260c9e2d8018692a05b4f133822651
gerardimitri <gtrincadomunoz@gmail.com>
2021-07-23 20:56:30 -0400

    OBJ.py y collisions.py agregados para hacerlo mas ordenado

---
+++ collisions.py
@@ -0,0 +1,226 @@
+import glfw
+from OpenGL.GL import *
+import OpenGL.GL.shaders
+import numpy as np
+import random
+import sys
+import os.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import grafica.transformations as tr
+import grafica.basic_shapes as bs
+import grafica.easy_shaders as es
+import grafica.lighting_shaders as ls
+import grafica.performance_monitor as pm
+from grafica.assets_path import getAssetPath
+
+NUMBER_OF_CIRCLES = 10
+CIRCLE_DISCRETIZATION = 20
+RADIUS = 0.08
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
+
+def createGPUShape(pipeline, shape):
+     # Funcion Conveniente para facilitar la inicializacion de un GPUShape
+    gpuShape = es.GPUShape().initBuffers()
+    pipeline.setupVAO(gpuShape)
+    gpuShape.fillBuffers(shape.vertices, shape.indices, GL_STATIC_DRAW)
+    return gpuShape
+
+def createTextureGPUShape(shape, pipeline, path):
+    # Funcion Conveniente para facilitar la inicializacion de un GPUShape con texturas
+    gpuShape = es.GPUShape().initBuffers()
+    pipeline.setupVAO(gpuShape)
+    gpuShape.fillBuffers(shape.vertices, shape.indices, GL_STATIC_DRAW)
+    gpuShape.texture = es.textureSimpleSetup(
+        path, GL_CLAMP_TO_EDGE, GL_CLAMP_TO_EDGE, GL_NEAREST, GL_NEAREST)
+    return gpuShape
+
+def createColorNormalSphere(N, r, g, b):
+    # Funcion para crear una esfera con normales
+
+    vertices = []           # lista para almacenar los verices
+    indices = []            # lista para almacenar los indices
+    dTheta = 2 * np.pi /N   # angulo que hay entre cada iteracion de la coordenada theta
+    dPhi = 2 * np.pi /N     # angulo que hay entre cada iteracion de la coordenada phi
+    rho = 0.5               # radio de la esfera
+    c = 0                   # contador de vertices, para ayudar a indicar los indices
+
+    # Se recorre la coordenada theta
+    for i in range(N - 1):
+        theta = i * dTheta # angulo theta en esta iteracion
+        theta1 = (i + 1) * dTheta # angulo theta en la iteracion siguiente
+        # Se recorre la coordenada phi
+        for j in range(N):
+            phi = j*dPhi # angulo phi en esta iteracion
+            phi1 = (j+1)*dPhi # angulo phi en la iteracion siguiente
+
+            # Se crean los vertices necesarios son coordenadas esfericas para cada iteracion
+
+            # Vertice para las iteraciones actuales de theta (i) y phi (j) 
+            v0 = [rho*np.sin(theta)*np.cos(phi), rho*np.sin(theta)*np.sin(phi), rho*np.cos(theta)]
+            # Vertice para las iteraciones siguiente de theta (i + 1) y actual de phi (j) 
+            v1 = [rho*np.sin(theta1)*np.cos(phi), rho*np.sin(theta1)*np.sin(phi), rho*np.cos(theta1)]
+            # Vertice para las iteraciones actual de theta (i) y siguiente de phi (j + 1) 
+            v2 = [rho*np.sin(theta1)*np.cos(phi1), rho*np.sin(theta1)*np.sin(phi1), rho*np.cos(theta1)]
+            # Vertice para las iteraciones siguientes de theta (i + 1) y phi (j + 1) 
+            v3 = [rho*np.sin(theta)*np.cos(phi1), rho*np.sin(theta)*np.sin(phi1), rho*np.cos(theta)]
+            
+            # Se crean los vectores normales para cada vertice segun los valores de rho tongo 
+            n0 = [np.sin(theta)*np.cos(phi), np.sin(theta)*np.sin(phi), np.cos(theta)]
+            n1 = [np.sin(theta1)*np.cos(phi), np.sin(theta1)*np.sin(phi), np.cos(theta1)]
+            n2 = [np.sin(theta1)*np.cos(phi1), np.sin(theta1)*np.sin(phi1), np.cos(theta1)]
+            n3 = [np.sin(theta)*np.cos(phi1), np.sin(theta)*np.sin(phi1), np.cos(theta)]
+
+
+            # Creamos los triangulos superiores
+            #        v0
+            #       /  \
+            #      /    \
+            #     /      \
+            #    /        \
+            #   /          \
+            # v1 ---------- v2
+            if i == 0:
+                #           vertices              color    normales
+                vertices += [v0[0], v0[1], v0[2], r, g, b, n0[0], n0[1], n0[2]]
+                vertices += [v1[0], v1[1], v1[2], r, g, b, n1[0], n1[1], n1[2]]
+                vertices += [v2[0], v2[1], v2[2], r, g, b, n2[0], n2[1], n2[2]]
+                indices += [ c + 0, c + 1, c +2 ]
+                c += 3
+
+            # Creamos los triangulos inferiores
+            # v0 ---------- v3
+            #   \          /
+            #    \        /
+            #     \      /
+            #      \    /
+            #       \  /
+            #        v1
+            elif i == (N-2):
+                #           vertices              color    normales
+                vertices += [v0[0], v0[1], v0[2], r, g, b, n0[0], n0[1], n0[2]]
+                vertices += [v1[0], v1[1], v1[2], r, g, b, n1[0], n1[1], n1[2]]
+                vertices += [v3[0], v3[1], v3[2], r, g, b, n3[0], n3[1], n3[2]]
+                indices += [ c + 0, c + 1, c +2 ]
+                c += 3
+            
+            # Creamos los quads intermedios
+            #  v0 -------------- v3
+            #  | \                |
+            #  |    \             |
+            #  |       \          |
+            #  |          \       |
+            #  |             \    |
+            #  |                \ |
+            #  v1 -------------- v2
+            else: 
+                #           vertices              color    normales
+                vertices += [v0[0], v0[1], v0[2], r, g, b, n0[0], n0[1], n0[2]]
+                vertices += [v1[0], v1[1], v1[2], r, g, b, n1[0], n1[1], n1[2]]
+                vertices += [v2[0], v2[1], v2[2], r, g, b, n2[0], n2[1], n2[2]]
+                vertices += [v3[0], v3[1], v3[2], r, g, b, n3[0], n3[1], n3[2]]
+                indices += [ c + 0, c + 1, c +2 ]
+                indices += [ c + 2, c + 3, c + 0 ]
+                c += 4
+    return bs.Shape(vertices, indices)
+
+class Circle:
+    def __init__(self, pipeline, position, velocity, r, g, b):
+        shape = createColorNormalSphere(CIRCLE_DISCRETIZATION, r, g, b)
+        # addapting the size of the circle's vertices to have a circle
+        # with the desired radius
+        scaleFactor = 2 * RADIUS
+        bs.scaleVertices(shape, 6, (scaleFactor, scaleFactor, 1.0))
+        self.pipeline = pipeline
+        self.gpuShape = createGPUShape(self.pipeline, shape)
+        self.position = position
+        self.radius = RADIUS
+        self.velocity = velocity
+
+    def action(self, gravityAceleration, deltaTime):
+        # Euler integration
+        self.velocity += deltaTime * gravityAceleration
+        self.position += self.velocity * deltaTime
+
+    def draw(self):
+        glUniformMatrix4fv(glGetUniformLocation(self.pipeline.shaderProgram, "transform"), 1, GL_TRUE,
+            tr.translate(self.position[0], self.position[1], 0.0)
+        )
+        self.pipeline.drawCall(self.gpuShape)
+    
+
+def rotate2D(vector, theta):
+    """
+    Direct application of a 2D rotation
+    """
+    sin_theta = np.sin(theta)
+    cos_theta = np.cos(theta)
+
+    return np.array([
+        cos_theta * vector[0] - sin_theta * vector[1],
+        sin_theta * vector[0] + cos_theta * vector[1]
+    ], dtype = np.float32)
+
+
+def collide(circle1, circle2):
+    """
+    If there are a collision between the circles, it modifies the velocity of
+    both circles in a way that preserves energy and momentum.
+    """
+    
+    assert isinstance(circle1, Circle)
+    assert isinstance(circle2, Circle)
+
+    normal = circle2.position - circle1.position
+    normal /= np.linalg.norm(normal)
+
+    circle1MovingToNormal = np.dot(circle2.velocity, normal) > 0.0
+    circle2MovingToNormal = np.dot(circle1.velocity, normal) < 0.0
+
+    if not (circle1MovingToNormal and circle2MovingToNormal):
+
+        # obtaining the tangent direction
+        tangent = rotate2D(normal, np.pi/2.0)
+
+        # Projecting the velocity vector over the normal and tangent directions
+        # for both circles, 1 and 2.
+        v1n = np.dot(circle1.velocity, normal) * normal
+        v1t = np.dot(circle1.velocity, tangent) * tangent
+
+        v2n = np.dot(circle2.velocity, normal) * normal
+        v2t = np.dot(circle2.velocity, tangent) * tangent
+
+        # swaping the normal components...
+        # this means that we applying energy and momentum conservation
+        circle1.velocity = v2n + v1t
+        circle2.velocity = v1n + v2t
+
+
+def areColliding(circle1, circle2):
+    assert isinstance(circle1, Circle)
+    assert isinstance(circle2, Circle)
+
+    difference = circle2.position - circle1.position
+    distance = np.linalg.norm(difference)
+    collisionDistance = circle2.radius + circle1.radius
+    return distance < collisionDistance
+
+
+def collideWithBorder(circle):
+
+    # Right
+    if circle.position[0] + circle.radius > 1.0:
+        circle.velocity[0] = -abs(circle.velocity[0])
+
+    # Left
+    if circle.position[0] < -1.0 + circle.radius:
+        circle.velocity[0] = abs(circle.velocity[0])
+
+    # Top
+    if circle.position[1] > 1.0 - circle.radius:
+        circle.velocity[1] = -abs(circle.velocity[1])
+
+    # Bottom
+    if circle.position[1] < -1.0 + circle.radius:
+        circle.velocity[1] = abs(circle.velocity[1])
+
--- pool_party.py
+++ pool_party.py
@@ -12,17 +12,19 @@ import grafica.easy_shaders as es
 import grafica.lighting_shaders as ls
 import grafica.performance_monitor as pm
 from grafica.assets_path import getAssetPath
+from OBJreader import *
 
 __author__ = "Gerardo Trincado"
 __license__ = "MIT"
 
-#A class to store the application control
+# A class to store the application control
 class Controller:
     def __init__(self):
         self.fillPolygon = True
+        self.circleCollisions = False
+        self.useGravity = False
 
-
-# We will use the global controller as communication with the callback function
+# we will use the global controller as communication with the callback function
 controller = Controller()
 
 
@@ -39,80 +41,30 @@ def on_key(window, key, scancode, action, mods):
     elif key == glfw.KEY_ESCAPE:
         glfw.set_window_should_close(window, True)
 
+# def on_key(window, key, scancode, action, mods):
 
-def readFaceVertex(faceDescription):
+#     if action != glfw.PRESS:
+#         return
+    
+#     global controller
 
-    aux = faceDescription.split('/')
+#     if key == glfw.KEY_SPACE:
+#         controller.fillPolygon = not controller.fillPolygon
+#         print("Fill polygons?", controller.fillPolygon)
 
-    assert len(aux[0]), "Vertex index has not been defined."
+#     elif key == glfw.KEY_ESCAPE:
+#         glfw.set_window_should_close(window, True)
 
-    faceVertex = [int(aux[0]), None, None]
+#     elif key == glfw.KEY_1:
+#         controller.circleCollisions = not controller.circleCollisions
+#         print("Collisions among circles?", controller.circleCollisions)
 
-    assert len(aux) == 3, "Only faces where its vertices require 3 indices are defined."
+#     elif key == glfw.KEY_2:
+#         controller.useGravity = not controller.useGravity
+#         print("Gravity?", controller.useGravity)
 
-    if len(aux[1]) != 0:
-        faceVertex[1] = int(aux[1])
-
-    if len(aux[2]) != 0:
-        faceVertex[2] = int(aux[2])
-
-    return faceVertex
-
-
-
-def readOBJ(filename):
-
-    vertices = []
-    normals = []
-    textCoords= []
-    faces = []
-
-    with open(filename, 'r') as file:
-        for line in file.readlines():
-            aux = line.strip().split(' ')
-            
-            if aux[0] == 'v':
-                vertices += [[float(coord) for coord in aux[1:]]]
-
-            elif aux[0] == 'vn':
-                normals += [[float(coord) for coord in aux[1:]]]
-
-            elif aux[0] == 'vt':
-                assert len(aux[1:]) == 2, "Texture coordinates with different than 2 dimensions are not supported"
-                textCoords += [[float(coord) for coord in aux[1:]]]
-
-            elif aux[0] == 'f':
-                N = len(aux)                
-                faces += [[readFaceVertex(faceVertex) for faceVertex in aux[1:4]]]
-                for i in range(3, N-1):
-                    faces += [[readFaceVertex(faceVertex) for faceVertex in [aux[i], aux[i+1], aux[1]]]]
-
-        vertexData = []
-        indices = []
-        index = 0
-
-        # Per previous construction, each face is a triangle
-        for face in faces:
-
-            # Checking each of the triangle vertices
-            for i in range(0,3):
-                vertex = vertices[face[i][0]-1]
-                texCoord = textCoords[face[i][1]-1]
-                normal = normals[face[i][2]-1]
-
-                vertexData += [
-                    vertex[0], vertex[1], vertex[2],
-                    texCoord[0], texCoord[1],
-                    normal[0], normal[1], normal[2]
-                ]
-
-            # Connecting the 3 vertices to create a triangle
-            indices += [index, index + 1, index + 2]
-            index += 3        
-
-        return bs.Shape(vertexData, indices)
-
-
+#     else:
+#         print('Unknown key')
 
 
 
@@ -244,7 +196,8 @@ if __name__ == "__main__":
         glUniform3f(glGetUniformLocation(phongPipeline.shaderProgram, "viewPosition"), viewPos[0], viewPos[1], viewPos[2])
         glUniformMatrix4fv(glGetUniformLocation(phongPipeline.shaderProgram, "view"), 1, GL_TRUE, view)
 
-        glUniformMatrix4fv(glGetUniformLocation(phongPipeline.shaderProgram, "model"), 1, GL_TRUE, tr.uniformScale(0.1))
+        glUniformMatrix4fv(glGetUniformLocation(phongPipeline.shaderProgram, "model"), 1, GL_TRUE, tr.matmul([
+            tr.translate(0,0,-7.5),tr.uniformScale(0.1)]))
         phongPipeline.drawCall(gpuPoolTable)
 
         
